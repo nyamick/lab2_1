f937667a458bc025275b475b154037eabacd2167
stud <stud@I1013465849>
2022-10-03 12:10:56 +0800

    Добавьте файлы проекта.

---
+++ app1.2/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace app1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(203, 36);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 23);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введие трехзначное число:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Получить ответ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 124);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtNumber;
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ app1.2/Form1.cs
@@ -0,0 +1,48 @@
+namespace app1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtNumber.Text = Properties.Settings.Default.number.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int number;
+            try
+            {
+                number = int.Parse(this.txtNumber.Text);
+            }
+            catch (FormatException)
+            {
+                return;
+            }
+            Properties.Settings.Default.number = number;
+            Properties.Settings.Default.Save();
+            var average =(Logic.Calculate(number));
+            MessageBox.Show("Наибольшая цифра трехзначного числа: " + average.ToString()) ;
+        }
+    }
+    public class Logic
+    {
+        public static int Calculate(int number)
+        {
+            var maxNumber = number / 10 % 10;
+
+            if (number / 100 > maxNumber)
+            {
+                maxNumber = number / 100;
+            }
+            if (number % 10 > maxNumber)
+            {
+                maxNumber = number % 10;
+            }
+
+            return (maxNumber);
+        }
+
+    }
+}
