af1f79c2380f8c611c1419a8a83bd7e72e14e172
nyamick <nyamick123@gmail.com>
2022-10-10 12:14:59 +0800

    FormCreation

--- app1.2/Form1.cs
+++ app1.2/Form1.cs
@@ -11,38 +11,8 @@ namespace app1._2
         private void button1_Click(object sender, EventArgs e)
         {
             
-            int number;
-            try
-            {
-                number = int.Parse(this.txtNumber.Text);
-            }
-            catch (FormatException)
-            {
-                return;
-            }
-            Properties.Settings.Default.number = number;
-            Properties.Settings.Default.Save();
-            var average =(Logic.Calculate(number));
-            MessageBox.Show("Наибольшая цифра трехзначного числа: " + average.ToString()) ;
+            
         }
     }
-    public class Logic
-    {
-        public static int Calculate(int number)
-        {
-            var maxNumber = number / 10 % 10;
-
-            if (number / 100 > maxNumber)
-            {
-                maxNumber = number / 100;
-            }
-            if (number % 10 > maxNumber)
-            {
-                maxNumber = number % 10;
-            }
-
-            return (maxNumber);
-        }
-
-    }
+    
 }
